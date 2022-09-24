86d350011025b5de85e94ad6a570b6c611ff72a8
Christian Meter <cmeter@googlemail.com>
2022-09-24 18:05:40 +0200

    Remove prismjs

--- posts/hello-world/index.md
+++ posts/hello-world/index.md
@@ -9,20 +9,3 @@
 
 MarkDoc is a superset of the [CommonMark Specification](https://commonmark.org).
 This document is a playground for testing our implementation.
-
-## Code Highlightng
-
-```javascript {% highlight="1,4" %}
-const name = "Luke Skywalker";
-
-function sayHello(name) {
-  console.log(`Hello ${name}!`);
-}
-```
-
-```clojure
-(def name "Luke Skywalker")
-
-(defn say-hello [name]
-  (println (str "Hello " name "!")))
-```
--- src/blog/core.cljs
+++ src/blog/core.cljs
@@ -3,8 +3,6 @@
   (:require ["@markdoc/markdoc$default" :as markdoc :refer [Tag]]
             ["@sindresorhus/slugify$default" :as slugify]
             ["path" :as path]
-            ["prism-react-renderer$default" :refer [Prism]]
-            ["prism-react-renderer$default.default" :as Highlight]
             ["react$default" :as React]
             ["zx" :refer [fs glob]]
             [applied-science.js-interop :as j]
@@ -21,44 +19,6 @@
 
 (defn date->human [date]
   (.toLocaleDateString date "de-DE" #js {:year "numeric" :month "long" :day "numeric"}))
-
-;; -----------------------------------------------------------------------------
-;; Highlight Code
-
-(defn add-prismjs-language [language]
-  (set! (.-Prism js/global) Prism)
-  ((js/require "prismjs/components/") language))
-
-(add-prismjs-language "clojure")
-
-(defn line-highlighted-fn? [highlight]
-  (let [lines (->> (str/split highlight #",")
-                   (map #(js/parseInt % 10))
-                   (set))]
-    (fn [n] (some #(= n %) lines))))
-
-(defn Fence [{:keys [language highlight content]}]
-  [:> Highlight {"Prism" Prism :code content :theme nil :language language}
-   (fn [params] (let [{tokens "tokens"
-                       class "className"
-                       style "style"
-                       get-line-props "getLineProps"
-                       get-token-props "getTokenProps"} (js->clj params)
-                      line-highlighted? (line-highlighted-fn? highlight)]
-                  (r/as-element [:div.relative>pre.grid {:class [class] :style style}
-                                 (map-indexed (fn [il line]
-                                                [:div
-                                                 (update (js->clj (get-line-props #js {:line line :key il}) :keywordize-keys true) :class conj (when (line-highlighted? (inc il)) "-mx-4 px-[0.7rem] border-l-4 border-yellow-400 bg-yellow-300/[0.25]"))
-                                                 (map-indexed (fn [it token] [:span (js->clj (get-token-props (clj->js {:token token :key it})) :keywordize-keys true)])
-                                                              line)]) tokens)
-                                 [:div.absolute.top-0.right-0.rounded-b-lg.bg-gray-600.text-xs.text-slate-200.p-2.mr-2.font-mono
-                                  {:style {:text-shadow "none" :line-height "0.2rem"}}
-                                  language]])))])
-
-(def node-fence {:render "Fence"
-                 :attributes {:content {:type 'String}
-                              :highlight {:type 'String}
-                              :language {:type 'String}}})
 
 ;; -----------------------------------------------------------------------------
 ;; Build Index Page
@@ -108,13 +68,11 @@
   (let [ast (markdoc/parse markdown)
         frontmatter (parse-frontmatter ast)
         rendertree (markdoc/transform ast (clj->js {:variables frontmatter
-                                                    :nodes {:heading node-heading
-                                                            :fence node-fence}}))
+                                                    :nodes {:heading node-heading}}))
         react-elements (markdoc/renderers.react
                         rendertree
                         React
-                        (clj->js {:components {"Heading" (r/reactify-component Heading)
-                                               "Fence" (r/reactify-component Fence)}}))]
+                        (clj->js {:components {"Heading" (r/reactify-component Heading)}}))]
     [react-elements frontmatter]))
 
 (defn make-templated-html [title content]
@@ -128,8 +86,7 @@
     [:dl
      [:dd.absolute.top-0.inset-x-0.text-slate-700
       [:time {:date-time (.toISOString date)} (date->human date)]]]]
-   [:div.prose.prose-slate.max-w-none content]
-   [:script {:src "https://utteranc.es/client.js" :repo "n2o/blog" "issue-term" "pathname" :theme "github-light" :cross-origin "anonymous" :async true}]])
+   [:div.prose.prose-slate.max-w-none content]])
 
 
 (defn process-post-path [post-path]
